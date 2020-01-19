7f7fdf3e9ee8901965c1569d5055fdacca9ebfdf
YUKIKO IKEDA <applelele@gmail.com>
2020-01-19 18:10:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,14 +1,14 @@
-# def mario
-#   phrase = "It's-a me, Mario!"
-#   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-#   puts phrase
-# end
-
-# def toadstool
-#   puts status
-# end
-
-def link
-  phrase = "It's Dangerous To Go Alone! Take This."
+def mario
+  phrase = "It's-a me, Mario!"
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
+
+def toadstool
+  puts status
+end
+
+# def link
+#   phrase = "It's Dangerous To Go Alone! Take This."
+#   puts phrase
+# end
